e938990bc54287e4f915d47f2ffbc906db9ca377
Soham <sohamjpathak@gmail.com>
2023-11-03 19:26:52 +0530

    First commit

--- src/main/java/com/driver/Car.java
+++ src/main/java/com/driver/Car.java
@@ -33,4 +33,33 @@ public class Car extends Vehicle {
         move(newSpeed,newDirection);
         System.out.println("changeSpeed method called - The speed is changed to: " + newSpeed + ", and the direction is changed to: " + newDirection + " degrees");
     }
+
+    public int getCurrentGear() {
+        return currentGear;
+    }
+
+    public boolean isManual() {
+        return isManual;
+    }
+
+    public int getDoors() {
+        return doors;
+    }
+
+    public int getGears() {
+        return gears;
+    }
+
+    public int getSeats() {
+        return seats;
+    }
+
+    public int getWheels() {
+        return wheels;
+    }
+
+    public String getType() {
+        return type;
+    }
+
 }
